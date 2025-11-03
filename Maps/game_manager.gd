62b1033e72e913dec7873865f4743947bad9ba99
Rian <redkrsc@gmail.com>
2025-11-03 16:25:56 -0500

    Fire begins in 3 minutes

---
+++ Maps/game_manager.gd
@@ -0,0 +1,26 @@
+class_name GameManager
+
+extends Node3D
+
+static var instance: GameManager 
+
+@export var cycle_time: float
+@export var meltdown_time: float
+
+var timer: float
+
+var meltdown_begun: bool
+
+
+func _enter_tree():
+	instance = self
+
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= cycle_time and not meltdown_begun:
+		meltdown_begun = true
+		var _fire_nodes = get_tree().get_nodes_in_group("FireFX")
+		for _node in _fire_nodes:
+			_node.begin_meltdown()
+	if timer >= cycle_time + meltdown_time:
+		$Bunraku.jumpscare()
--- Scripts/Environment/fire_effects.gd
+++ Scripts/Environment/fire_effects.gd
@@ -1,21 +1,26 @@
 extends Node3D
 
-@export var meltdown_length: float = 5
 @export var fire_material: ShaderMaterial
 @export var fire_individual_material: ShaderMaterial
+@export var environment: WorldEnvironment
 
 var counter = 0
 var meltdown_in_progress: bool = false
-func _ready() -> void:
-	meltdown_in_progress = true
+var meltdown_length: float = 5
 
+func _ready() -> void:
+	meltdown_length = GameManager.instance.meltdown_time
+	
 func _process(delta: float) -> void:
 	if meltdown_in_progress:
 		counter = move_toward(counter, meltdown_length, delta)
 		set_fire_progress(counter/meltdown_length)
 	
 func begin_meltdown():
-	create_tween().tween_method(set_fire_progress, 0, 1, meltdown_length)
+	meltdown_in_progress = true
+	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.volume_linear = 0
+	create_tween().tween_property($AudioStreamPlayer, "volume_linear", 1.5, meltdown_length)
 
 func set_fire_progress(t: float):
 	fire_material.set_shader_parameter("fire_alpha", t / 2)
@@ -23,3 +28,5 @@ func set_fire_progress(t: float):
 	fire_individual_material.set_shader_parameter("fire_alpha", t / 2)
 	$Firelight1.light_energy = t*1.5
 	$Firelight2.light_energy = t*16
+	environment.environment.fog_density = t/2
+	environment.environment.fog_light_color.r = t/5
