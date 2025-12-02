e76353b0d4b2b7e97db023c08d93885c447372a4
Si Yue Jiang <113212500+melancholive@users.noreply.github.com>
2025-12-01 23:57:22 -0500

    shring offering logic

--- Scripts/Interactables/shrine.gd
+++ Scripts/Interactables/shrine.gd
@@ -7,6 +7,20 @@ var first_viewing: bool = true
 @export var inspect_fov: float = 20.0
 @onready var focus_marker: Node3D = $FocusMarker
 
+@export var left_table: FruitTable
+@export var right_table: FruitTable
+
+#func check_offerings():
+	#if not left_table.has_fruit or not right_table.has_fruit:
+		#return false  # One or both tables empty
+#
+	#if left_table.fruit_type == "apple" and right_table.fruit_type == "peach":
+		#print("Both offerings are correct")
+		#return true
+	#else:
+		#print("Offerings incorrect. Left:", left_table.fruit_type, "Right:", right_table.fruit_type)
+		#return false
+
 func can_interact() -> bool:
 	return InspectionManager.current_mode == InspectionManager.Mode.PLAY
 
@@ -16,11 +30,16 @@ func on_interact():
 		InspectionManager.enter_inspect(self, focus_marker, inspect_fov)
 		get_viewport().set_input_as_handled()
 		
-		dialogue_id = "left_submission" if randf() > 0.5 else "right_submission"
-
+		#dialogue_id = "left_submission" if randf() > 0.5 else "right_submission"
+		if left_table.has_fruit and right_table.has_fruit:
+			if  left_table.fruit_type == "apple" and right_table.fruit_type == "peach":
+				dialogue_id = "success"
+			else: 
+				dialogue_id = "failure"
+			
 		await get_tree().create_timer(0.1).timeout
 		if dialogue_id != "" and first_viewing:
-			first_viewing = false
+			#first_viewing = false
 			var _dialogue_box: DialogueBox = DialogueBox.instance
 			_dialogue_box.data = environmental_dialogues
 			_dialogue_box.start(dialogue_id)
