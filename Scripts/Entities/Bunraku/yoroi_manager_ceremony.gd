2675e8113fc7fd264e19fad147ce6f65446973f8
Rian <redkrsc@gmail.com>
2025-12-14 17:04:04 -0500

    Yoroi drinking from cups

--- Scripts/Entities/Bunraku/yoroi_manager_ceremony.gd
+++ Scripts/Entities/Bunraku/yoroi_manager_ceremony.gd
@@ -7,6 +7,8 @@ extends BunrakuManager
 @export var normal_light_color: Color
 @export var evil_light_color: Color
 
+@export var sake_games: Array[DrunkYoroiGame] 
+@export var drunkness: float
 var turn_time_counter: float
 var physics_delta: float
 var orig_y: float
@@ -42,15 +44,33 @@ func _physics_process(_delta):
 			$Yoroi.active = false
 			$YoroiBackside.visible = true
 			$"../../Objects/Chandelier/OmniLight3D".light_color = normal_light_color
+			$Yoroi/Feedback2.stop()
 			
+			var _sake_games_filled: Array[DrunkYoroiGame]
+			for game in sake_games:
+				if game.get_node("SakazukiCup").current_fill_level > 0:
+					_sake_games_filled.append(game)
+			
+			if _sake_games_filled.is_empty():
+				position.z = randf_range(-1.4, -6.4)
+				position.x = randf_range(1, 5)	
+			else:
+				_sake_games_filled.shuffle()
+				var _the_game: DrunkYoroiGame = _sake_games_filled[0]
+				global_position.z = _the_game.global_position.z
+				global_position.x = _the_game.global_position.x
+				position = position.move_toward( Vector3(3.193, 0.373, -4.598), 0.6 )
+				drunkness += _the_game.get_node("SakazukiCup").current_fill_level
+				$Slurp.play()
+				_the_game.get_node("SakazukiCup").empty_cup()
 		else:
 			looking_forward = true
 			$Yoroi.visible = true
 			$Yoroi.active = true
 			$YoroiBackside.visible = false
 			$"../../Objects/Chandelier/OmniLight3D".light_color = evil_light_color
+			$Yoroi/Feedback2.play()
 			
-	
 func light_flicker():
 	var _distance_to_player = global_position.distance_to(Player.instance.global_position)
 	var _do_light_flicker: bool = false
